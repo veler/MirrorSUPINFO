b1099398b4bbadbdd09c18097d6b2676ce17ea23
crazy <crazy@DESKTOP-67UH8RH>
2016-05-01 22:12:10 +0200

    IconButton Done. Start Icons Dictionary

---
+++ MirrorSUPINFO/Sources/Libraries/MirrorSUPINFO.Design/Controls/IconButton.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace MirrorSUPINFO.Design.Controls
+{
+    public partial class IconButton : UserControl
+    {
+        public IconButton()
+        {
+            this.InitializeComponent();
+        }
+
+        public ImageSource Image
+        {
+            get { return (ImageSource)GetValue(ImageProperty); }
+            set { SetValue(ImageProperty, value); }
+        }
+
+        public static readonly DependencyProperty ImageProperty = DependencyProperty.Register(
+            "Image", typeof(ImageSource), typeof(IconButton), new PropertyMetadata(null)
+        );
+
+        public double ImageWidth
+        {
+            get { return (double)GetValue(ImageWidthProperty); }
+            set { SetValue(ImageWidthProperty, value); }
+        }
+
+        public static readonly DependencyProperty ImageWidthProperty = DependencyProperty.Register(
+            "ImageWidth", typeof(double), typeof(IconButton), new PropertyMetadata(16d)
+        );
+
+        public double ImageHeight
+        {
+            get { return (double)GetValue(ImageHeightProperty); }
+            set { SetValue(ImageHeightProperty, value); }
+        }
+
+        public static readonly DependencyProperty ImageHeightProperty = DependencyProperty.Register(
+            "ImageHeight", typeof(double), typeof(IconButton), new PropertyMetadata(16d)
+        );
+    }
+}
