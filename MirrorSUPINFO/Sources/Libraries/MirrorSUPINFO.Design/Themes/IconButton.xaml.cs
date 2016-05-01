10eccb5a5f599535bb2a6f50f8cbbb159ee0a867
crazy <crazy@DESKTOP-67UH8RH>
2016-05-01 17:00:23 +0200

    Done : IconButton

---
+++ MirrorSUPINFO/Sources/Libraries/MirrorSUPINFO.Design/Themes/IconButton.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace MirrorSUPINFO.Design.Themes
+{
+    public partial class IconButton : UserControl
+    {
+        public event RoutedEventHandler Click;
+
+        public IconButton()
+        {
+            InitializeComponent();
+        }
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public static readonly DependencyProperty TextProperty =
+          DependencyProperty.Register("Text", typeof(string), typeof(IconButton), new UIPropertyMetadata(""));
+
+        public ImageSource Image
+        {
+            get { return (ImageSource)GetValue(ImageProperty); }
+            set { SetValue(ImageProperty, value); }
+        }
+
+        public static readonly DependencyProperty ImageProperty =
+           DependencyProperty.Register("Image", typeof(ImageSource), typeof(IconButton), new UIPropertyMetadata(null));
+        
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            if (null != Click)
+                Click(sender, e);
+        }
+    }
+}
