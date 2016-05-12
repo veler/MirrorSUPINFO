7088f797e02e6d2a0e431fb97fe19c24d6a06d6f
crazy <crazy@DESKTOP-67UH8RH>
2016-05-12 22:41:13 +0200

    Adding TextBlock Control

---
+++ MirrorSUPINFO/Sources/Libraries/MirrorSUPINFO.Design/Controls/TextBlock.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace MirrorSUPINFO.Design.Controls
+{
+    public partial class TextBlock : UserControl
+    {
+        public TextBlock()
+        {
+            this.InitializeComponent();
+        }
+
+        public string Text
+        {
+            get { return (string)GetValue(TextContent); }
+            set { SetValue(TextContent, value); }
+        }
+
+        public static readonly DependencyProperty TextContent = DependencyProperty.Register("Text", typeof(string), typeof(TextBlock), new PropertyMetadata(null));
+    }
+}
