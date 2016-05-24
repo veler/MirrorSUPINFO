b95eb1eef3e9cac4ea1eafda0e8cc1d67dbedce5
krazu_000 <krazu_000@CRAZYBLUE>
2016-05-24 16:20:44 +0200

    ListView Start,

--- MirrorSUPINFO/Sources/Libraries/MirrorSUPINFO.Design/Controls/IconButton.xaml.cs
+++ MirrorSUPINFO/Sources/Libraries/MirrorSUPINFO.Design/Controls/IconButton.xaml.cs
@@ -1,25 +1,22 @@
-﻿using Windows.UI.Xaml;
+﻿using System.ComponentModel;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media;
+using MirrorSUPINFO.SDK.PathConverter;
 
 namespace MirrorSUPINFO.Design.Controls
 {
     public partial class IconButton : UserControl
     {
+        PropertiesHolder propHolder;
+
         public IconButton()
         {
+            propHolder = new PropertiesHolder();
+
             this.InitializeComponent();
+            this.DataContext = propHolder;
         }
-
-        public ImageSource Image
-        {
-            get { return (ImageSource)GetValue(ImageProperty); }
-            set { SetValue(ImageProperty, value); }
-        }
-
-        public static readonly DependencyProperty ImageProperty = DependencyProperty.Register(
-            "Image", typeof(ImageSource), typeof(IconButton), new PropertyMetadata(null)
-        );
 
         public double ImageWidth
         {
@@ -40,5 +37,64 @@ namespace MirrorSUPINFO.Design.Controls
         public static readonly DependencyProperty ImageHeightProperty = DependencyProperty.Register(
             "ImageHeight", typeof(double), typeof(IconButton), new PropertyMetadata(16d)
         );
+
+        public string ImageKey
+        {
+            get
+            {
+                var property = this.Resources[ImageKeyProperty].ToString();
+                return Convert(property);
+            }
+            set
+            {
+                propHolder.pathText = this.Resources[value].ToString();
+            }
+        }
+
+        public static readonly DependencyProperty ImageKeyProperty = DependencyProperty.Register("ImageKey", typeof(string), typeof(IconButton), new PropertyMetadata(default(string)));
+
+        private static string Convert(string value)
+        {
+            if (value == null)
+            {
+                return null;
+            }
+
+            var converter = new StringToPathGeometryConverter();
+            var convertString = converter.Convert(value);
+            var pathText = converter.ConvertBack(convertString);
+
+            return pathText?.Length > 0 ? pathText : null;
+        }
+
+
+        public class PropertiesHolder : INotifyPropertyChanged
+        {
+            public string pathText
+            {
+                get { return _pathText; }
+                set
+                {
+                    _pathText = value;
+                    OnPropertyChanged("pathText");
+                }
+            }
+
+            string _pathText = default(string);
+
+            #region INotifyPropertyChanged Members
+
+            public event PropertyChangedEventHandler PropertyChanged;
+
+            public void OnPropertyChanged(string name)
+            {
+                if (PropertyChanged != null)
+                    PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+
+            #endregion
+        }
+
+
     }
 }
